ce049f7cde9f9867f9cda366d23d73c2f504afdd
Vanshi <preprojectdev@gmail.com>
2020-04-20 01:52:44 +0530

    Initial commit

---
+++ dateranagepicker/src/main/java/com/date/dateranagepicker/CalendarCellDecorator.java
@@ -0,0 +1,7 @@
+package com.date.dateranagepicker;
+
+import java.util.Date;
+
+public interface CalendarCellDecorator {
+  void decorate(CalendarCellView cellView, Date date);
+}
---
+++ dateranagepicker/src/main/java/com/date/dateranagepicker/DayViewAdapter.java
@@ -0,0 +1,6 @@
+package com.date.dateranagepicker;
+
+/** Adapter used to provide a layout for {@link CalendarCellView}.*/
+public interface DayViewAdapter {
+  void makeCellView(CalendarCellView parent);
+}
