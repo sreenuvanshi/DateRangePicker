a11c1e5b210e01b84432b7c12c61274c7ad7b6d8
Vanshi <preprojectdev@gmail.com>
2020-04-20 02:12:49 +0530

    merge

--- app/src/main/java/com/daterangepicker/MainActivity.kt
+++ app/src/main/java/com/daterangepicker/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.daterangepicker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.date.dateranagepicker.CalendarPickerView
+import com.date.dateranagepicker.CalendarPickerView.SelectionMode.RANGE
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val calendar: CalendarPickerView = findViewById(R.id.calendar_view)
+        val nextYear = Calendar.getInstance()
+        nextYear.add(Calendar.YEAR, 1)
+        val today = Date()
+        calendar.init(today, nextYear.time)
+                .withSelectedDate(today)
+        calendar.init(today, nextYear.time)
+                .inMode(RANGE)
     }
 }
